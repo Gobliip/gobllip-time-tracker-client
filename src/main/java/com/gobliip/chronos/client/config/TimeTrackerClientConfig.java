2bac7bfbbceba2734bea2b2e24d4c0a38e615565
Leonel Samayoa <leonel.samayoa@xoom.com>
2015-07-15 15:41:31 -0600

    Extracted retrofit-cloud

--- src/main/java/com/gobliip/chronos/client/config/TimeTrackerClientConfig.java
+++ src/main/java/com/gobliip/chronos/client/config/TimeTrackerClientConfig.java
@@ -2,15 +2,19 @@ package com.gobliip.chronos.client.config;
 
 import com.gobliip.chronos.client.TimeTrackerClient;
 import com.gobliip.chronos.client.retrofit.TimeTrackerResource;
-import com.gobliip.retrofit.oauth2.InMemoryJWTTokenStore;
-import com.gobliip.retrofit.oauth2.JWTTokenStore;
-import com.gobliip.retrofit.oauth2.OAuthJWTTokenInterceptor;
+import com.gobliip.retrofit.cloud.endpoint.RoundRobinEndpoint;
+import com.gobliip.retrofit.cloud.oauth2.jwt.InMemoryJWTTokenStore;
+import com.gobliip.retrofit.cloud.oauth2.jwt.JWTTokenStore;
+import com.gobliip.retrofit.cloud.oauth2.jwt.OAuthJWTTokenInterceptor;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit.Endpoint;
+import retrofit.Endpoints;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
 
@@ -20,7 +24,10 @@ import retrofit.converter.GsonConverter;
 @Configuration
 public class TimeTrackerClientConfig {
 
-    @Value("${gobliip.time-tracker.endpoint}")
+    @Value("${gobliip.time-tracker.service-name:time-tracker}")
+    private String serviceName;
+
+    @Value("${gobliip.time-tracker.endpoint:http://time-tracker.gobliip.:5022/}")
     private String endpoint;
 
     @Autowired(required = false)
@@ -28,6 +35,9 @@ public class TimeTrackerClientConfig {
 
     @Autowired(required = false)
     private OAuthJWTTokenInterceptor jwtTokenInterceptor;
+
+    @Autowired(required = false)
+    private DiscoveryClient discoveryClient;
 
     @Bean
     public TimeTrackerClient timeTrackerClient(){
@@ -42,10 +52,17 @@ public class TimeTrackerClientConfig {
     public RestAdapter restAdapter() {
         final RestAdapter restAdapter = new RestAdapter.Builder()
                 .setConverter(new GsonConverter(gson()))
-                .setEndpoint(endpoint)
+                .setEndpoint(timeTrackerEndpoint())
                 .setRequestInterceptor(jwtTokenInterceptor())
                 .build();
         return restAdapter;
+    }
+
+    @Bean
+    public Endpoint timeTrackerEndpoint() {
+        // We try to use service discovery first
+        if(discoveryClient != null) return new RoundRobinEndpoint(discoveryClient, serviceName);
+        return Endpoints.newFixedEndpoint(this.endpoint);
     }
 
     @Bean
